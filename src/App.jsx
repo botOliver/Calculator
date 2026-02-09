5128dadfd9c4d81bb45873a49e48361038e70fe1
botOliver <botoliver556@gmail.com>
2026-02-09 15:44:11 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -25,40 +25,43 @@ import './index.css'
 //Example: eval("console.log('Hello')") will execute this code.
 // Use this method for all operations in the calculator.
 
-// function incrementPlus() {
-// 	setDisplayInput(prev => prev + 1)
-// }
-
 function Calculator() {
-	const [displayInput, setDisplayInput] = useState(0)
+	const [displayInput, setDisplayInput] = useState('0')
 
-	function equals(equals) {
-		setDisplayInput(eval(equals))
+	function plusOne() {
+		const current = eval(displayInput)
+		setDisplayInput(current + 1).toString()
 	}
 
-	function changeDisplayOperator(changeOperator) {
-		setDisplayInput(prev => prev + changeOperator)
+	function minusOne() {
+		const current = eval(displayInput)
+		setDisplayInput(current - 1).toString()
 	}
 
-	function changeDisplayInput(changeNumber) {
-		setDisplayInput(prev => (prev == 0 ? changeNumber : prev + changeNumber))
+	function changeDisplayInput(changeValue) {
+		if (changeValue === 'C') {
+			setDisplayInput('0')
+			return
+		} else if (changeValue === '=') {
+			setDisplayInput(eval(displayInput)).toString()
+			return
+		}
+		setDisplayInput(prev => (prev === '0' ? changeValue : prev + changeValue))
+		return
 	}
+
+	console.log(displayInput)
+	console.log(typeof displayInput)
 	return (
 		<div className='calculator-container'>
 			<h1 className='calculator-title'>UseState Calculator</h1>
 			<div className='calculator'>
 				<div className='display'>{displayInput}</div>
 				<div className='increment-buttons'>
-					<button
-						className='increment'
-						onClick={() => setDisplayInput(prev => prev + 1)}
-					>
+					<button className='increment' onClick={() => plusOne()}>
 						+1
 					</button>
-					<button
-						className='decrement'
-						onClick={() => setDisplayInput(prev => prev - 1)}
-					>
+					<button className='decrement' onClick={() => minusOne()}>
 						-1
 					</button>
 				</div>
@@ -66,37 +69,28 @@ function Calculator() {
 					<button onClick={() => changeDisplayInput('7')}>7</button>
 					<button onClick={() => changeDisplayInput('8')}>8</button>
 					<button onClick={() => changeDisplayInput('9')}>9</button>
-					<button
-						className='operator'
-						onClick={() => changeDisplayOperator('+')}
-					>
+					<button className='operator' onClick={() => changeDisplayInput('+')}>
 						+
 					</button>
 					<button onClick={() => changeDisplayInput('4')}>4</button>
 					<button onClick={() => changeDisplayInput('5')}>5</button>
 					<button onClick={() => changeDisplayInput('6')}>6</button>
-					<button onClick={() => changeDisplayOperator('-')}>-</button>
+					<button onClick={() => changeDisplayInput('-')}>-</button>
 					<button onClick={() => changeDisplayInput('1')}>1</button>
 					<button onClick={() => changeDisplayInput('2')}>2</button>
 					<button onClick={() => changeDisplayInput('3')}>3</button>
-					<button
-						className='operator'
-						onClick={() => changeDisplayOperator('*')}
-					>
+					<button className='operator' onClick={() => changeDisplayInput('*')}>
 						×
 					</button>
 					<button onClick={() => changeDisplayInput('0')}>0</button>
-					<button onClick={() => changeDisplayOperator('.')}>,</button>
-					<button className='equals' onClick={() => equals(displayInput)}>
+					<button onClick={() => changeDisplayInput('.')}>,</button>
+					<button className='equals' onClick={() => changeDisplayInput('=')}>
 						=
 					</button>
-					<button
-						className='operator'
-						onClick={() => changeDisplayOperator('/')}
-					>
+					<button className='operator' onClick={() => changeDisplayInput('/')}>
 						÷
 					</button>
-					<button className='clear' onClick={() => setDisplayInput('0')}>
+					<button className='clear' onClick={() => changeDisplayInput('C')}>
 						C
 					</button>
 				</div>
